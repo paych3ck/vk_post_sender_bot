575a7b36394599cfd5b32156eea0ac92974253c1
Andrey Kataev <kata3v.andrey@yandex.ru>
2022-06-04 23:44:51 +0300

    PEP8.

--- mailbot.py
+++ mailbot.py
@@ -5,15 +5,16 @@ import requests
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from config import group_token, phone_number, password, chat_list, group_id
 
+
 def handler_function():
     code = input("Код: ")
     auth = True
     return code, auth
 
-vk = vk_api.VkApi(token = group_token)
+vk = vk_api.VkApi(token=group_token)
 longpoll = VkBotLongPoll(vk, group_id)
 
-vk_session = vk_api.VkApi(phone_number, password, auth_handler = handler_function)
+vk_session = vk_api.VkApi(phone_number, password, auth_handler=handler_function)
 vk_session.auth()
 vk_phone = vk_session.get_api()
 
@@ -22,7 +23,7 @@ print("Авторизация пройдена.")
 try:
     for event in longpoll.listen():
         if event.type == VkBotEventType.WALL_POST_NEW:
-            new_post = vk_phone.wall.get(owner_id = -group_id, count = 1)
+            new_post = vk_phone.wall.get(owner_id=-group_id, count=1)
             new_post_id = new_post["items"][0].get("id")
             attachment_formed = f"wall-{str(group_id}_{str(new_post_id)}"
 
